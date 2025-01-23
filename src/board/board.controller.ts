647cccf53d476f79d8c38023a27087e3673bd595
lgm-7 <xlzlrud@naver.com>
2025-01-23 20:33:19 +0900

    :반짝임: update 보드컨트롤러추가

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,19 +1,57 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { BoardService } from './board.service';
 import { update } from 'lodash';
+import { BoardDto } from './dto/board.dto';
 
 @Controller('board')
 export class BoardController {
-    constructor(private readonly boardService: BoardService){}
+  constructor(private readonly boardService: BoardService) {}
 
-    @Get(':boardId')
+  @Get(':boardId') // 보드 상세조회
+  async getBoard(@Param('boardId') boardId: number) {
+    return await this.boardService.getBoard(boardId);
+  }
 
-    @Get('')
+  @Get('') //내 보드들 조회
+  async getMyBoards(@UserInfo() user: User) {
+    return await this.boardService.getMyBoards(user.id);
+  }
 
-    @Post('')
+  @Post('') //보드 생성
+  async createBoard(@UserInfo() user: User, @Body() boardDto: BoardDto) {
+    await this.boardService.createBoard(
+      user.id,
+      boardDto.title,
+      boardDto.content,
+      boardDto.expriyDate,
+    );
+  }
 
-    @Patch('')
+  @Patch(':boardId')
+  async updateBoard(
+    @UserInfo() user: User,
+    @Param('boardId') boardId: number,
+    @Body() boardDto: BoardDto,
+  ) {
+    return await this.boardService.updateBoard(
+      boardId,
+      user.id,
+      boardDto.title,
+      boardDto.content,
+      boardDto.expriyDate,
+    );
+  }
 
-    @Delete('')
-
+  @Delete(':boardId')
+  async deleteBoard(@UserInfo() user: User, @Param('boardId') boardId: number) {
+    return await this.boardService.deleteBoard(boardId, user.id);
+  }
 }
