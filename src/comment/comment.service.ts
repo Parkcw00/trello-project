5b12e53303f5fc3de934a188cfc2f57580dd1b7b
Parkcw00 <parkcw0325@gmail.com>
2025-01-24 15:48:22 +0900

    일시정지

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import _ from 'lodash';
+import { Comment } from './entities/comment.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CommentService {}
