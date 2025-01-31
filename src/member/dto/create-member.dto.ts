b1a81b901e609664abd4209b276fb06cbb1117ca
강나연 <skdusdl4@naver.com>
2025-01-31 14:34:43 +0900

    ✨ update / 멤버 파일 작성

--- src/member/dto/create-member.dto.ts
+++ src/member/dto/create-member.dto.ts
@@ -1,1 +1,7 @@
-export class CreateMemberDto {}
+import { IsNotEmpty, IsNumber } from 'class-validator';
+
+export class CreateMemberDto {
+  @IsNotEmpty({ message: 'userId는 필수 입력값입니다.' }) // 빈 값이면 오류 발생
+  @IsNumber({}, { message: 'userId는 숫자여야 합니다.' }) // 숫자 타입만 허용
+  userId: number;
+}
