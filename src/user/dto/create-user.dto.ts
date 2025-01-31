58b13593625aed51eb44bcda927be64ea8ae713a
yoon yewon <yoonstrobus@gmail.com>
2025-01-31 20:37:03 +0900

    fix dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,19 @@
+
+import { User } from '../entities/user.entity';
+import { IsNumber, IsString } from 'class-validator'; // 데코레이터 가져오기
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  
+   @IsString()
   name: string;
+
+   @IsString()
   email: string;
+
+   @IsString()
   password: string;
+
+   @IsString()
   verifyPassword: string;
 }
