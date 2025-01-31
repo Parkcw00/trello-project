118aca9de016ad94f6993168f61ec684fc2800c1
강나연 <skdusdl4@naver.com>
2025-01-31 16:41:51 +0900

    ✨ update / 스웨거 사용 로직 추가

--- src/member/dto/create-member.dto.ts
+++ src/member/dto/create-member.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateMemberDto {
+  @ApiProperty({ example: 1 })
   @IsNotEmpty({ message: 'userId는 필수 입력값입니다.' }) // 빈 값이면 오류 발생
   @IsNumber({}, { message: 'userId는 숫자여야 합니다.' }) // 숫자 타입만 허용
   userId: number;
--- src/member/member.controller.ts
+++ src/member/member.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { MemberService } from './member.service';
 import { CreateMemberDto } from './dto/create-member.dto';
 import { UpdateMemberDto } from './dto/update-member.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 // 'board/:boardId/members' 경로에서 멤버 관련 API 요청을 처리하는 컨트롤러
+@ApiTags('멤버CRUD')
 @Controller('board/:boardId/members')
 export class MemberController {
   constructor(private readonly memberService: MemberService) {}
@@ -12,6 +22,7 @@ export class MemberController {
    * 특정 보드에 속한 모든 멤버를 조회하는 API
    * @ param boardId - 조회할 보드의 ID (URL에서 가져옴)
    */
+  @ApiOperation({ summary: '전체 멤버 조회' })
   @Get()
   findAll(@Param('boardId') boardId: number) {
     return this.memberService.findAll(boardId);
@@ -22,8 +33,12 @@ export class MemberController {
    * @ param boardId - 멤버가 속한 보드의 ID (URL에서 가져옴)
    * @ param memberId - 조회할 멤버의 ID (URL에서 가져옴)
    */
+  @ApiOperation({ summary: '특정 멤버 조회' })
   @Get(':memberId')
-  findOne(@Param('boardId') boardId: number, @Param('memberId') memberId: number) {
+  findOne(
+    @Param('boardId') boardId: number,
+    @Param('memberId') memberId: number,
+  ) {
     return this.memberService.findOne(boardId, memberId);
   }
 
@@ -32,8 +47,12 @@ export class MemberController {
    * @ param boardId - 멤버를 추가할 보드의 ID (URL에서 가져옴)
    * @ param createMemberDto - { userId } (Body에서 유저 ID를 받음)
    */
+  @ApiOperation({ summary: '멤버 추가' })
   @Post()
-  create(@Param('boardId') boardId: number, @Body() createMemberDto: CreateMemberDto) {
+  create(
+    @Param('boardId') boardId: number,
+    @Body() createMemberDto: CreateMemberDto,
+  ) {
     return this.memberService.create(boardId, createMemberDto);
   }
 
@@ -42,8 +61,12 @@ export class MemberController {
    * @ param boardId - 멤버가 속한 보드의 ID (URL에서 가져옴)
    * @ param memberId - 삭제할 멤버의 ID (URL에서 가져옴)
    */
+  @ApiOperation({ summary: '멤버 삭제' })
   @Delete(':memberId')
-  delete(@Param('boardId') boardId: number, @Param('memberId') memberId: number) {
+  delete(
+    @Param('boardId') boardId: number,
+    @Param('memberId') memberId: number,
+  ) {
     return this.memberService.delete(boardId, memberId);
   }
 }
