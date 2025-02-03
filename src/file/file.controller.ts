30a9a4e34a8750a5feb50047de09b22eb203161e
김지웅 <sizera1004@gmail.com>
2025-02-03 15:51:10 +0900

    🩹 fix: file.controller에 인가설정

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,16 +1,33 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile, Res, NotFoundException } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFile,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { FileService } from './file.service';
 import { FileUploadInterceptor } from './interceptors/file-upload.interceptor';
 import { DeleteFileDto } from './dto/delete-file.dto';
 import { Response } from 'express';
+import { AuthGuard } from '@nestjs/passport';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('cards/:id/file')
 export class FileController {
   constructor(private readonly fileService: FileService) {}
 
   @Post('upload')
   @UseInterceptors(FileUploadInterceptor)
-  async uploadFile(@Param('id') id: string, @UploadedFile() file: Express.Multer.File) {
+  async uploadFile(
+    @Param('id') id: string,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
     return this.fileService.uploadFile(id, file);
   }
 
@@ -20,15 +37,18 @@ export class FileController {
   }
 
   @Delete('')
-  async deleteFile(@Param('id') id: string, @Body() deleteFileDto: DeleteFileDto) {
-    return this.fileService.deleteFile(id, deleteFileDto)
+  async deleteFile(
+    @Param('id') id: string,
+    @Body() deleteFileDto: DeleteFileDto,
+  ) {
+    return this.fileService.deleteFile(id, deleteFileDto);
   }
 
   @Get('download/:fileName')
   async downloadFile(
     @Param('id') id: string,
     @Param('fileName') fileName: string,
-    @Res() res: Response
+    @Res() res: Response,
   ) {
     return this.fileService.downloadFile(id, fileName, res);
   }
