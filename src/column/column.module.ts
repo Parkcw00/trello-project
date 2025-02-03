1a494cc931bbffc956e7209bca784bb4e44852bc
ohhyeseong <ohs020105@gmail.com>
2025-02-03 16:14:58 +0900

    fix / userGard 사용해 인증 인가 기능 실행

--- src/column/column.module.ts
+++ src/column/column.module.ts
@@ -3,20 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm'; // 타입ORM 모듈 가져오�
 import { ColumnEntity } from './entities/column.entity'; // 엔티티 가져오기
 import { ColumnService } from './column.service'; // 서비스 가져오기
 import { ColumnController } from './column.controller'; // 컨트롤러 가져오기
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
-import { AuthMiddleware } from '../auth/auth.middleware'; // 인증 미들웨어 임포트
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([ColumnEntity]),
-    JwtModule.registerAsync({
-      useFactory: (config: ConfigService) => ({
-        secret: config.get<string>('JWT_SECRET_KEY'),
-      }),
-      inject: [ConfigService],
-    }),
-  ],
+  imports: [TypeOrmModule.forFeature([ColumnEntity])],
   controllers: [ColumnController], // 컨트롤러 등록
   providers: [ColumnService], // 서비스 등록
 })
--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ async function bootstrap() {
     .addServer('https://staging.yourapi.com/', 'Staging')
     .addServer('https://production.yourapi.com/', 'Production')
     .addTag('Your API Tag')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
@@ -29,3 +30,4 @@ async function bootstrap() {
 }
 
 bootstrap();
+
