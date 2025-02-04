85fbd9e25351bd4c189e773f58a99d56eceaedcf
김지웅 <sizera1004@gmail.com>
2025-02-04 15:48:39 +0900

    ✨ update: checklist.module코드 작성

--- src/checklist/checklist.module.ts
+++ src/checklist/checklist.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ChecklistService } from './checklist.service';
 import { ChecklistController } from './checklist.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Checklist } from './entities/checklist.entity';
+import { Member } from 'src/member/entities/member.entity';
+import { Board } from 'src/board/entities/board.entity';
+import { Card } from 'src/card/entities/card.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Checklist, Member, Board, Card])],
   controllers: [ChecklistController],
   providers: [ChecklistService],
 })
