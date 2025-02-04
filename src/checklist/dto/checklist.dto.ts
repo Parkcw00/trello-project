165d992a02849f3da8223ab53a04fc7901ac9519
김지웅 <sizera1004@gmail.com>
2025-02-04 15:46:31 +0900

    🩹 fix: checklist.dto파일에 클래스명 수정

--- src/checklist/dto/checklist.dto.ts
+++ src/checklist/dto/checklist.dto.ts
@@ -3,7 +3,7 @@ import { Checklist } from "../entities/checklist.entity";
 import { IsString } from "class-validator";
 
 
-export class DeleteFileDto extends PickType(Checklist, ['content']) {
+export class ChecklistDto extends PickType(Checklist, ['content']) {
     @IsString()
     content: string;
 }
